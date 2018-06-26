dfda15f0cb37ac24561e3b74dfadd21b3cebae46
cevcode <e.kobernik@web-spectre.ru>
2018-06-26 15:38:10 +0300

    added 2 sliders

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slick-carousel/slick/slick.js',
 		'app/js/common.min.js', // Always end
 		])
 	.pipe(concat('scripts.min.js'))
