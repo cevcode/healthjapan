0c8ba03563d3911e22e501f2ace68f96980e5d69
cevcode <e.kobernik@web-spectre.ru>
2018-06-27 16:27:36 +0300

    added some logic on header

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,42 @@
-$(function() {
+function toggleDropDown(button, id) {
+    $(button).click(function() {
+        event.preventDefault();
+        var elem = $(id);
+        if(elem.hasClass('show')) {
+            elem.removeClass('show');
+        }
+        else
+        {
+            elem.addClass('show');
+        }
+    });
+    var inside = false;
+    $(button).click(function()
+    {
+        $('.dropdown').hover(function(){
+            inside=true;
+        }, function(){
+            inside=false;
+        });
 
-	// Custom JS
+        $("body").mouseup(function(){
+            if(!inside) $('.dropdown').removeClass('show');
+        });
+    });
+};
 
-});
+$('#search').focus(function () {
+    $('.search__result').show();
+})
+$('#search').blur(function () {
+    $('.search__result').hide();
+})
+
+
+
+toggleDropDown('#cart-btn', '#cart-result');
+toggleDropDown('#favorite-btn', '#favorite-result');
+toggleDropDown('#compare-btn', '#compare-result');
 
 $('.slider').slick({
     infinite: true,
@@ -24,5 +58,3 @@ $('.new-offers__slider').slick({
     slidesToScroll: 5,
     dots: true
 });
-
-
